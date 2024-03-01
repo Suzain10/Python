c3dc0d672ebfbd31ef18cc71a7baa60b0e835e5b
suzainrafiq <suzain900@gmail.com>
2024-03-01 10:12:33 +0530

    Learning File IO Operations

---
+++ File IO.py
@@ -0,0 +1,14 @@
+##Reading a file:
+
+##f = open('myfile.txt','r')
+##text = f.read()
+##print(text)
+##f.close()
+
+##Writing to a file:
+f = open('myfile2.txt', 'a')
+f.write('It is March')
+f.close()
+
+with open('myfile2.txt','a') as f:
+ f.write("Hey I am inside with\n")
